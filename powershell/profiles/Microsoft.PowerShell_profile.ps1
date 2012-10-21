f9cd8d07c12e01cbd47e52219bef8a9447cc0f5f
Stephen Edgar <stephen@netweb.com.au>
2012-10-21 19:13:30 +1100

    Call ~\.dotfiles\powershell\profiles\.profile.ps1

---
+++ powershell/profiles/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+﻿##############################################################################################
+# Load PowerShell Profile .profile.ps1 - https://github.com/ntwb/dotfiles
+# Called from C:\Users\Stephen\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 
+##############################################################################################
+
+. '~\.dotfiles\powershell\profiles\.profile.ps1'
