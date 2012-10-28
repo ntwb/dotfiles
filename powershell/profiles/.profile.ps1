26620c08c0a02e9ff82e16d41df35f313720b210
Stephen Edgar <stephen@netweb.com.au>
2012-10-28 13:02:37 +1100

    Python for Windows 2.7.3

--- powershell/profiles/.profile.ps1
+++ powershell/profiles/.profile.ps1
@@ -16,6 +16,10 @@ $env:Path = $env:Path + ";C:\Users\Stephen\AppData\Roaming\npm"
 # PHP for Windows http://php.net/
 $env:Path = $env:Path + ";C:\PHP"
 
+# Python for Windows 2.7.3 http://www.python.org/download/
+$env:Path = $env:Path + ";C:\Python27"
+# Python scripts inside same PowerShell window http://mycomputeradventures.blogspot.com.au/2012/09/python-scripts-inside-powershell-window.html
+$env:PATHEXT += ";.py"
 
 # Load posh-git example profile
 . 'H:\dev\_git\posh-git\profile.example.ps1'
