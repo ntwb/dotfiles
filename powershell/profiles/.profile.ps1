2be0ce4506b4b5f95cf6984b6fa6acdc2a83283f
Stephen Edgar <stephen@netweb.com.au>
2012-10-28 13:02:08 +1100

    Inline docs, links etc

--- powershell/profiles/.profile.ps1
+++ powershell/profiles/.profile.ps1
@@ -3,10 +3,17 @@
 # Called from C:\Users\Stephen\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 
 ##############################################################################################
 
+Write-Host -ForegroundColor Green "--> Setting up enviroment variables..."
+
+# GitHub for Windows https://windows.github.com/
 $env:Path = $env:Path + ";C:\Users\Stephen\AppData\Local\GitHub\PortableGit_8810fd5c2c79c73adcc73fd0825f3b32fdb816e7\cmd"
 $env:Path = $env:Path + ";C:\Users\Stephen\AppData\Local\GitHub\PortableGit_8810fd5c2c79c73adcc73fd0825f3b32fdb816e7\bin"
+
+# NodeJS x64 for Windows http://nodejs.org/
 $env:Path = $env:Path + ";C:\Program Files\nodejs"
 $env:Path = $env:Path + ";C:\Users\Stephen\AppData\Roaming\npm"
+
+# PHP for Windows http://php.net/
 $env:Path = $env:Path + ";C:\PHP"
 
 
