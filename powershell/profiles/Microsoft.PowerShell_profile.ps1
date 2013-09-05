1826d39c4a402e07a55dd008d2fec3957e105dfc
Stephen Edgar <stephen@netweb.com.au>
2013-09-06 08:08:06 +1000

    Change background when elevated

--- powershell/profiles/Microsoft.PowerShell_profile.ps1
+++ powershell/profiles/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,17 @@
 ﻿##############################################################################################
 # Load PowerShell Profile .profile.ps1 - https://github.com/ntwb/dotfiles
-# Called from C:\Users\Stephen\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 
+# Called from C:\Users\Stephen\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 ##############################################################################################
-
+# red background if running elevated
+& {
+ $wid=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+ $prp=new-object System.Security.Principal.WindowsPrincipal($wid)
+ $adm=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+ $IsAdmin=$prp.IsInRole($adm)
+ if ($IsAdmin)
+ {
+  (get-host).UI.RawUI.Backgroundcolor="DarkRed"
+  clear-host
+ }
+}
 . '~\.dotfiles\powershell\profiles\.profile.ps1'
