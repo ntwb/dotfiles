58a38dbffcbfa3a3d77248be2b827d22c123544c
Stephen Edgar <stephen@netweb.com.au>
2021-01-03 20:24:49 +1100

    Initial dotfiles command alias in `fish.config`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,6 @@ function ..    ; cd .. ; end
 function ...   ; cd ../.. ; end
 function ....  ; cd ../../.. ; end
 function ..... ; cd ../../../.. ; end
+
+# Dotfiles config
+alias dotfiles='/usr/bin/git --git-dir=/Users/dotfiles/.dotfiles/ --work-tree=/Users/dotfiles'
