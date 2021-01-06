bc63a73d5ef874ef91c81f0a70110fce9ae357c9
Stephen Edgar <stephen@netweb.com.au>
2021-01-06 19:39:58 +1100

    Add Hyper `hyper-monokai-pro` plugin

--- .hyper.js
+++ .hyper.js
@@ -131,7 +131,9 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: [],
+    plugins: [
+		'hyper-monokai-pro', // https://www.npmjs.com/package/hyper-monokai-pro
+	],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
